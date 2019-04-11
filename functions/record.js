d0f36f784f6441d4aced9f04e2771dd68d43e8b7
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-04-11 19:14:30 +0900

    fix: recorderでないlistenerから着信があった場合は団体名は名乗るよう修正

--- functions/record.js
+++ functions/record.js
@@ -4,22 +4,25 @@ exports.handler = function(context, event, callback) {
   };
   const twiml = new Twilio.twiml.VoiceResponse();
 
-  const file = Runtime.getAssets()['recorders.js'].path;
-  const recorders = require(file);
+  let message = '';
+  const recorders = require(Runtime.getAssets()['recorders.js'].path);
+  const listeners = require(Runtime.getAssets()['listeners.js'].path);
+
+  if (listeners.includes(event.From)) {
+    const organization = process.env.ORGANIZATION || '電話連絡網';
+    message += `こちらは、${organization}です。`;
+  }
   if (!recorders.includes(event.From)) {
-    twiml.say(`申し訳ありません。この電話は、特定の番号からの着信のみ受け付けています。`, opt);
+    message += `この電話には、特定の番号からの着信のみ受け付けています。`;
+    twiml.say(message, opt);
     twiml.hangup();
     callback(null, twiml);
     return;
   }
 
-  const organization = process.env.ORGANIZATION || '電話連絡網';
-  twiml.say(
-    `こんにちは。${organization}です。
-	ピーという発信音の後に、メッセージを録音してください。
-	電話を切ると、関係者に録音内容が転送されます。`,
-    opt
-  );
+  message += `ピーという発信音の後に、メッセージを録音してください。
+	電話を切ると、関係者に録音内容が転送されます。`;
+  twiml.say(message, opt);
 
   const Called = encodeURIComponent(event.Called);
   const Caller = encodeURIComponent(event.From);
