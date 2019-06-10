ca4addded337cc2c961d99e7c05f8f8ab19c5da1
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-06-10 22:46:02 +0900

    refactor: async即時関数ではなく全体をasync関数に

--- functions/replay.js
+++ functions/replay.js
@@ -1,4 +1,4 @@
-exports.handler = function(context, event, callback) {
+exports.handler = async function(context, event, callback) {
   const opt = {
     language: 'ja-JP',
     voice: 'Polly.Mizuki',
@@ -6,31 +6,27 @@ exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
   const client = context.getTwilioClient();
 
-  (async () => {
-    // TODO: 最新3件くらいは聞けるようにしたい
-    const recording = await client.recordings.list({ limit: 1 }).then(_ => _[0]);
-    const RecordingUrl = `https://api.twilio.com/2010-04-01/Accounts/${context.ACCOUNT_SID}/Recordings/${
-      recording.sid
-    }`;
-    const recordingCall = await client.calls(recording.callSid).fetch();
-    const Recorder = recordingCall.from;
+  // TODO: 最新3件くらいは聞けるようにしたい
+  const recording = await client.recordings.list({ limit: 1 }).then(_ => _[0]);
+  const RecordingUrl = `https://api.twilio.com/2010-04-01/Accounts/${context.ACCOUNT_SID}/Recordings/${recording.sid}`;
+  const recordingCall = await client.calls(recording.callSid).fetch();
+  const Recorder = recordingCall.from;
 
-    const recorders = require(Runtime.getAssets()['recorders.js'].path);
-    const name = recorders[Recorder];
+  const recorders = require(Runtime.getAssets()['recorders.js'].path);
+  const name = recorders[Recorder];
 
-    const { format } = require('date-fns');
-    const { convertToTimeZone } = require('date-fns-timezone');
-    const ja = require('date-fns/locale/ja');
-    const date = convertToTimeZone(recording.dateUpdated, { timeZone: 'Asia/Tokyo' });
-    const dateForSay = format(date, 'MMMDo A h時m分', { locale: ja });
+  const { format } = require('date-fns');
+  const { convertToTimeZone } = require('date-fns-timezone');
+  const ja = require('date-fns/locale/ja');
+  const date = convertToTimeZone(recording.dateUpdated, { timeZone: 'Asia/Tokyo' });
+  const dateForSay = format(date, 'MMMDo A h時m分', { locale: ja });
 
-    twiml.say(`最新の録音、${dateForSay}、${name}からのお知らせを再生します。`, opt);
-    twiml.pause();
-    twiml.play(RecordingUrl);
-    twiml.pause();
-    twiml.say(`メッセージは以上です。`, opt);
-    twiml.pause();
+  twiml.say(`最新の録音、${dateForSay}、${name}からのお知らせを再生します。`, opt);
+  twiml.pause();
+  twiml.play(RecordingUrl);
+  twiml.pause();
+  twiml.say(`メッセージは以上です。`, opt);
+  twiml.pause();
 
-    callback(null, twiml);
-  })();
+  callback(null, twiml);
 };
