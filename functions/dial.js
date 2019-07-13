35ca38cbf459c19e08ad8ec377856e98b261adc0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2019-07-13 22:46:59 +0900

    fix: Cannot set headers after they are sent to the client を防止

--- functions/dial.js
+++ functions/dial.js
@@ -16,17 +16,13 @@ exports.handler = function(context, event, callback) {
       },
       (err, result) => {
         if (err) {
-          console.log(err);
-          callback(err);
+          console.error(err);
           return;
         }
         console.log('Created calls using callback');
         console.log(result.sid);
-
-        // 少し待機してから発信（うまく動いてない？）
-        twiml.pause({ length: 10 });
-        callback(null, twiml);
       }
     );
   });
+  callback(null, twiml);
 };
