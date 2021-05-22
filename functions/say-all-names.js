4a3e104d1e32b80cf37dfbb45f602e11ab8c88d1
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-05-22 15:32:31 +0900

    fix: 発音チェック やっぱりrecordersに戻した

--- functions/say-all-names.js
+++ functions/say-all-names.js
@@ -5,13 +5,13 @@ exports.handler = function(context, event, callback) {
   };
   const twiml = new Twilio.twiml.VoiceResponse();
 
-  const listeners = require(Runtime.getAssets()['listeners.js'].path);
-  const listenersCount = Object.keys(listeners).length;
+  const recorders = require(Runtime.getAssets()['recorders.js'].path);
+  const recordersCount = Object.keys(recorders).length;
 
-  twiml.say(`関係者${listenersCount}人の、発音チェックを行います。`, opt);
+  twiml.say(`関係者${recordersCount}人の、発音チェックを行います。`, opt);
   twiml.pause();
 
-  Object.values(listeners).forEach(name => {
+  Object.values(recorders).forEach(name => {
     twiml.say(`${name}からのお知らせです。`, opt);
     twiml.pause();
   });
