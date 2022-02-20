79bbfeb8b8433d042bc0b63362df3fee2d85e2d0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-02-20 16:41:02 +0900

    fix: dial時のERR_HTTP_HEADERS_SENTエラーの解消
    callback()が宛先の数分だけ呼ばれてしまっていたので、
    全体通して1回だけ呼ばれるように修正

--- functions/dial.js
+++ functions/dial.js
@@ -9,23 +9,21 @@ exports.handler = async function(context, event, callback) {
   // 一斉配信前に3秒待つ（録音した人自身が一呼吸おけるように）
   await new Promise(r => setTimeout(r, 3000));
 
-  Object.keys(listeners).forEach(toNumber => {
-    client.calls.create(
-      {
-        url: `https://${context.DOMAIN}/play?Recorder=${Recorder}&RecordingUrl=${RecordingUrl}`,
-        method: 'GET',
-        to: toNumber,
-        from: context.CALLER_ID,
-      },
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          return callback(err);
-        }
-        console.log('Created calls using callback');
-        console.log(result.sid);
-        callback(null, twiml);
-      }
-    );
-  });
+  const promises = Object.keys(listeners).map(toNumber =>
+    client.calls.create({
+      url: `https://${context.DOMAIN}/play?Recorder=${Recorder}&RecordingUrl=${RecordingUrl}`,
+      method: 'GET',
+      to: toNumber,
+      from: context.CALLER_ID,
+    })
+  );
+  try {
+    const results = await Promise.allSettled(promises);
+    console.log('Created calls using callback');
+    console.log(results.map(_ => _.sid).join(','));
+    callback(null, twiml);
+  } catch (err) {
+    console.log(err);
+    callback(err);
+  }
 };
